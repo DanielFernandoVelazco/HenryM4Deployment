5704fc93f962753457f94cb1547e38bd63abd393
Fernando Velazco <daniel.fernando.velazco@gmail.com>
2024-08-08 23:34:23 -0500

    Se realiza la actividad 02 NestJS Auth

---
+++ back/e-commerce/src/auth/dto/signup-auth.dto.ts
@@ -0,0 +1,42 @@
+/* eslint-disable prettier/prettier */
+import { IsEmail, IsNotEmpty, IsOptional, IsString, Matches, MaxLength, MinLength } from "class-validator";
+
+export class SignUpAuthDto {
+    @MaxLength(80)
+    @MinLength(3)
+    @IsNotEmpty()
+    @IsString()
+    name: string;
+
+    @IsEmail()
+    email: string;
+
+    @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,15}$/, {
+        message: 'La contraseña debe contener al menos una letra mayúscula, un número, un caracter especial y debe tener entre 8 y 15 caracteres'
+    })
+    password: string;
+
+    @IsNotEmpty()
+    @IsString()
+    passwordConfirm: string;
+
+    @IsString()
+    address: string;
+
+    @IsNotEmpty()
+    @IsString()
+    phone: string;
+
+    @IsString()
+    @IsOptional()
+    country?: string;
+
+    @IsString()
+    @IsOptional()
+    city?: string;
+
+    //@IsString()
+    //@IsOptional()
+    //createdAt: string;
+}
+
