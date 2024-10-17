08bb6f3e37a254b2fb1a4358c14eca72489beda3
Fernando Velazco <daniel.fernando.velazco@gmail.com>
2024-10-16 19:34:06 -0500

    Se realiza la actividad 001 NestJS & TypeORM

---
+++ back/ecommerce-daniel-fernando-velazco/src/config/data-source.ts
@@ -0,0 +1,23 @@
+import { DataSource, DataSourceOptions } from "typeorm";
+import * as dotenv from "dotenv";
+import { registerAs } from "@nestjs/config";
+
+dotenv.config({
+    path: '.env.development',
+});
+
+const postgresDataSource: DataSourceOptions = {
+
+    type: 'postgres',
+    host: process.env.POSTGRES_HOST,
+    port: parseInt(process.env.POSTGRES_PORT, 10),
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DATABASE,
+    synchronize: true,
+    logging: true,
+    entities: ['dist/**/*.entity{.ts,.js}'],
+    migrations: ['dist/migrations/*{.ts,.js}'],
+};
+
+export const postgresDataSourceConfig = registerAs('postgres', () => postgresDataSource)
