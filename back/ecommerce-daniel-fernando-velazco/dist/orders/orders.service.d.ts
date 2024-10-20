c13064e29162fdad643afae3310507a76cd5c2a9
Fernando Velazco <daniel.fernando.velazco@gmail.com>
2024-10-19 21:31:09 -0500

    Se realiza la actividad 003 NestJS & TypeORM

---
+++ back/ecommerce-daniel-fernando-velazco/dist/orders/orders.service.d.ts
@@ -0,0 +1,21 @@
+import { CreateOrderDto } from './dto/create-order.dto';
+import { UpdateOrderDto } from './dto/update-order.dto';
+import { Order } from './entities/order.entity';
+import { Repository } from 'typeorm';
+import { UsersService } from 'src/users/users.service';
+import { ProductsService } from 'src/products/products.service';
+import { OrderDetailsService } from 'src/order-details/order-details.service';
+import { OrderResponseDto } from './dto/response-order.dto';
+export declare class OrdersService {
+    private readonly orderRepository;
+    private readonly usersService;
+    private readonly productsService;
+    private readonly ordderDetailsService;
+    constructor(orderRepository: Repository<Order>, usersService: UsersService, productsService: ProductsService, ordderDetailsService: OrderDetailsService);
+    create(createOrderDto: CreateOrderDto): Promise<OrderResponseDto>;
+    private calculateTotal;
+    findAll(): Promise<string>;
+    findOne(id: string): Promise<import("../order-details/entities/order-detail.entity").OrdersDetail[]>;
+    update(id: string, updateOrderDto: UpdateOrderDto): Promise<string>;
+    remove(id: string): Promise<string>;
+}
---
+++ back/ecommerce-daniel-fernando-velazco/src/orders/dto/response-order.dto.ts
@@ -0,0 +1,31 @@
+import { OrdersDetail } from 'src/order-details/entities/order-detail.entity';
+
+export class OrderResponseDto {
+
+    id: string
+
+    price: number
+
+    products: object[]
+
+    order: {
+        id: string
+        date: Date
+        user: {
+            id: string
+        }
+    }
+
+    constructor(orderDetail: OrdersDetail) {
+        this.id = orderDetail.id
+        this.price = orderDetail.price
+        this.products = orderDetail.products
+        this.order = {
+            id: orderDetail.order.id,
+            date: orderDetail.order.date,
+            user: {
+                id: orderDetail.order.user.id,
+            },
+        }
+    }
+}
