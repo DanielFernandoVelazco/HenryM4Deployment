89e75df45c8523fb4b8dd2b47a908b079288e536
Fernando Velazco <daniel.fernando.velazco@gmail.com>
2024-10-30 16:34:55 -0500

    Se realiza la actividad 004 - NestJS Auth II

--- back/ecommerce-daniel-fernando-velazco/dist/main.js
+++ back/ecommerce-daniel-fernando-velazco/dist/main.js
@@ -6,10 +6,13 @@ const logger_middleware_1 = require("./middleware/logger/logger.middleware");
 const categories_seeds_1 = require("./seeds/categories/categories.seeds");
 const products_seeds_1 = require("./seeds/products/products.seeds");
 const common_1 = require("@nestjs/common");
+const express_openid_connect_1 = require("express-openid-connect");
+const auth0_config_1 = require("./config/auth0-config");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     app.useGlobalPipes(new common_1.ValidationPipe());
     app.use(logger_middleware_1.globalLogger);
+    app.use((0, express_openid_connect_1.auth)({ ...auth0_config_1.auth0Config }));
     const categoriesSeed = app.get(categories_seeds_1.CategoriesSeed);
     await categoriesSeed.seed();
     console.log('*** LA INSERCION DE CATEGORIAS FUE EXITOSA ***');
