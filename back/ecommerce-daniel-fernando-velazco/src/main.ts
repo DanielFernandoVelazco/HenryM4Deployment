3bca985191526870004e4db732b3db55c4acf2f2
Fernando Velazco <daniel.fernando.velazco@gmail.com>
2024-10-30 16:33:12 -0500

    Se realiza la actividad 002 - NestJS Auth II

--- back/ecommerce-daniel-fernando-velazco/src/main.ts
+++ back/ecommerce-daniel-fernando-velazco/src/main.ts
@@ -4,11 +4,16 @@ import { globalLogger } from './middleware/logger/logger.middleware';
 import { CategoriesSeed } from './seeds/categories/categories.seeds';
 import { ProductsSeed } from './seeds/products/products.seeds';
 import { ValidationPipe } from '@nestjs/common';
+import { auth } from 'express-openid-connect';
+import { auth0Config } from './config/auth0-config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.use(globalLogger);
+  app.use(
+    auth({ ...auth0Config })
+  );
 
   const categoriesSeed = app.get(CategoriesSeed);
   await categoriesSeed.seed();
