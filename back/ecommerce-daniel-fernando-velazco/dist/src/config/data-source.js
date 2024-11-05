c2f4872a230f07be2634dfc49e0adc1bbe3e9e88
Fernando Velazco <daniel.fernando.velazco@gmail.com>
2024-11-04 23:53:16 -0500

    Se realiza la actividad 003 - Docker

--- back/ecommerce-daniel-fernando-velazco/dist/src/config/data-source.js
+++ back/ecommerce-daniel-fernando-velazco/dist/src/config/data-source.js
@@ -9,11 +9,11 @@ dotenv.config({
 });
 const PostgresDataSourceOptions = {
     type: 'postgres',
-    host: process.env.POSTGRES_HOST,
-    port: parseInt(process.env.POSTGRES_PORT, 10),
-    username: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_DATABASE,
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT, 10),
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     synchronize: true,
     logging: true,
     entities: ['dist/**/*.entity{.ts,.js}'],
